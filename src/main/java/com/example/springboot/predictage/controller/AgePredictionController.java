713b9b2430424706e8b8fb5ddd832fd92180bfe1
Semih <Semih Trkmenolu@semih>
2022-06-06 00:00:49 +0300

    add controller package

--- src/main/java/com/example/springboot/predictage/controller/AgePredictionController.java
+++ src/main/java/com/example/springboot/predictage/controller/AgePredictionController.java
@@ -1,4 +1,4 @@
-package com.example.springboot.predictage;
+package com.example.springboot.predictage.controller;
 
 import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
